20ab7af6b393ed2175fe798c309cae8bfc0b7ba1
Vitor Capretz <capretzvitor@gmail.com>
2022-05-14 10:27:52 -0300

    Initial Commit

---
+++ Shared/ConcentrationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationGameApp.swift
+//  Shared
+//
+//  Created by Vitor Capretz on 14/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
